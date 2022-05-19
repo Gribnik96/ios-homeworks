d0354ebed97c1b853dfcea662f1a6100b86ca813
Nikita Gribin <nikita.gribin@icloud.com>
2022-05-19 18:02:35 +0300

    Initial Commit

---
+++ Netology_IB_Instruments_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments_1
+//
+//  Created by Nikita Gribin on 19.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
